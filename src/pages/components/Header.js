deffde2151092c7fb4ef147dde26a07c9773e14a
yiritani <74131902+yiritani@users.noreply.github.com>
2023-11-05 12:55:36 +0900

    sticky header

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -6,47 +6,48 @@ import {HomeIcon} from "@heroicons/react/solid";
 export default function Header() {
   return (
     <>
-      <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
-        <div className='h-24 w-24 relative hidden lg:inline-grid cursor-pointer'>
-          <Image
-            src="https://links.papareact.com/ocw"
-            layout="fill"
-            className={"object-contain"}
-            alt={"instagram icon"}
-          />
-        </div>
-        <div className='h-24 w-10 relative lg:hidden cursor-pointer'>
-          <Image
-            src={"https://links.papareact.com/jjm"}
-            layout="fill"
-            className={"object-contain"}
-            alt={"instagram camera icon"}
-          />
-        </div>
-
-        <div className={"flex items-center relative"}>
-          <input
-            type="text"
-            className={"bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black"}
-            placeholder={"Search"}
-          />
-          <SearchIcon className={"h-6 w-6 text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/2"} />
-        </div>
-
-        <div className={"flex space-x-4 items-center"}>
-          <HomeIcon className={"hidden lg:inline-flex h-6 w-6 text-gray-500 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"} />
-          <PlusCircleIcon className={"h-6 w-6 text-gray-500 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"} />
-          <div className='h-10 w-10 relative cursor-pointer'>
+      <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
+        <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
+          <div className='h-24 w-24 relative hidden lg:inline-grid cursor-pointer'>
             <Image
-              src={"https://links.papareact.com/3ke"}
+              src="https://links.papareact.com/ocw"
               layout="fill"
-              className={"object-contain rounded-full"}
-              alt={"instagram plus icon"}
+              className={"object-contain"}
+              alt={"instagram icon"}
             />
+          </div>
+          <div className='h-24 w-10 relative lg:hidden cursor-pointer'>
+            <Image
+              src={"https://links.papareact.com/jjm"}
+              layout="fill"
+              className={"object-contain"}
+              alt={"instagram camera icon"}
+            />
+          </div>
+
+          <div className={"flex items-center relative"}>
+            <input
+              type="text"
+              className={"bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black"}
+              placeholder={"Search"}
+            />
+            <SearchIcon className={"h-6 w-6 text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/2"} />
+          </div>
+
+          <div className={"flex space-x-4 items-center"}>
+            <HomeIcon className={"hidden lg:inline-flex h-6 w-6 text-gray-500 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"} />
+            <PlusCircleIcon className={"h-6 w-6 text-gray-500 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"} />
+            <div className='h-10 w-10 relative cursor-pointer'>
+              <Image
+                src={"https://links.papareact.com/3ke"}
+                layout="fill"
+                className={"object-contain rounded-full"}
+                alt={"instagram plus icon"}
+              />
+            </div>
           </div>
         </div>
       </div>
-
 
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,6 @@ export default function Home() {
       <Header />
 
 
-
-
       </div>
   )
 }
