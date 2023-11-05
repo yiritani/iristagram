867aace9c8f3d5ea3eb3988e0d8a86c9f15701ff
yiritani <74131902+yiritani@users.noreply.github.com>
2023-11-05 14:34:36 +0900

    create post

--- src/pages/components/Post.jsx
+++ src/pages/components/Post.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import {DotsHorizontalIcon} from "@heroicons/react/solid";
 
 
 export default function Post({id, username, userImg, img, caption}) {
   return (
     <>
-      <div>
-        <h1>{username}</h1>
+    <div className={'bg-white my-7 border '}>
+      <div className={'flex items-center p-5'}>
+        <img
+          src={userImg}
+          alt={username}
+          className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+        />
+        <p className={'flex-1 w-12  font-bold cursor-pointer hover:underline'}>{username}</p>
+        <DotsHorizontalIcon className={'h-5 cursor-pointer'}/>
+      </div>
         <img
           src={img}
           alt={username}
