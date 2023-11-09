396eac06ded5f7ebe36cf90d9d34b5fa6f4ab978
yiritani <74131902+yiritani@users.noreply.github.com>
2023-11-09 20:48:46 +0900

    modal

--- src/components/UploadModal.jsx
+++ src/components/UploadModal.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import {useRecoilState} from "recoil";
 import {modalState} from "../../atom/modalAtom";
+import Modal from "react-modal";
 
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
 
-
   return (
     <>
       {open && (
-        <div className={"fixed z-10 inset-0 overflow-y-auto"}>
-          <div className={"flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"}>
-            <div className={"fixed inset-0 transition-opacity"} aria-hidden={"true"}>
-              <div className={"absolute inset-0 bg-gray-500 opacity-75"}></div>
-            </div>
-            <span className={"hidden sm:inline-block sm:align-middle sm:h-screen"} aria-hidden={"true"}>&#8203;</span>
+        <Modal
+          className={"max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white shadow-2xl rounded-md"}
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+        >
+          <div className={"flex items-center justify-between h-[100%] p-5"}>
+            <img
+              src={'https://www.freeiconspng.com/uploads/camera-icon-circle-21.png'}
+              alt={''}
+              className={"object-contain h-12 items-center justify-center"}
+            />
           </div>
-        </div>
+        </Modal>
 
       )}
 
